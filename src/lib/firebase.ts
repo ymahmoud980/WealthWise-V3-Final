de72eb4844ca38d9a500daa4801aeae81639e059
Yasser Mahmoud <YMahmoud1980@gmail.com>
2025-08-30 18:14:22 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "wealth-navigator-ab3ww",
+  "appId": "1:814157293824:web:c4d1071e5871d120a76be5",
+  "storageBucket": "wealth-navigator-ab3ww.firebasestorage.app",
+  "apiKey": "AIzaSyAjVlNb3BDMnApRWhGLLJxaNFefARHmZR8",
+  "authDomain": "wealth-navigator-ab3ww.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "814157293824"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export { app };
