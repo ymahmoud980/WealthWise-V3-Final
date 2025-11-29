52f82d7617f8bd33faf94f79015ef74c06f3b60f
Yasser Mahmoud <YMahmoud1980@gmail.com>
2025-11-29 17:41:17 +0000

    please connect this application to Firebase Console Authentication with

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,11 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBuezWec1n1B5wi7xGLatgQ3Ausmtb345g",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: "wealthwise-v3-final",
-  storageBucket: "wealthwise-v3-final.firebasestorage.app",
-  messagingSenderId: "489414271056",
-  appId: "1:489414271056:web:4c706525ef8a2b0149f155",
-  measurementId: "G-PG5LJCYPNS"
+  authDomain: "web-archive-harvester.firebaseapp.com",
+  projectId: "web-archive-harvester",
+  storageBucket: "web-archive-harvester.appspot.com",
+  messagingSenderId: "536596374039",
+  appId: "1:536596374039:web:ca213c8c6159457e123252",
 };
 
 // Initialize Firebase (Prevent double initialization in Next.js)
